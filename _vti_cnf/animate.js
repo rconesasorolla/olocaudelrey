ec50e047ac18c52d368986629040b8f952f53c3f
Raúl Conesa <raul@photopills.com>
2025-03-17 23:51:43 +0100

    Initial commit

---
+++ _vti_cnf/animate.js
@@ -0,0 +1,7 @@
+vti_encoding:SR|utf8-nl
+vti_author:SR|***
+vti_modifiedby:SR|***
+vti_timecreated:TR|05 Mar 2000 11:10:06 -0000
+vti_timelastmodified:TR|05 Mar 2000 11:10:06 -0000
+vti_backlinkinfo:VX|colada_canaleta_a_predonda.htm
+vti_extenderversion:SR|4.0.2.2717
---
+++ index_archivos/_vti_cnf/script.js
@@ -0,0 +1,4 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|29 Feb 2000 21:35:16 -0000
+vti_extenderversion:SR|4.0.2.2717
+vti_backlinkinfo:VX|index_archivos/master04.htm index_archivos/slide0001.htm index_archivos/master03.htm
